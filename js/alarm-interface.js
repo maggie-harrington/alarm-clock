9a7ad115b2f88c65a8be99ff190fdd2851036912
Jayeson Kiyabu and Maggie Harrington <maggie@>
2017-03-14 14:09:55 -0700

    sound alarm at inputed time

--- js/alarm-interface.js
+++ js/alarm-interface.js
@@ -1,14 +1,21 @@
 // var Calculator = require('./../js/alarm.js').calculatorModule;
 
 $(document).ready(function(){
-  var currentTime = $('#time').text(moment());
-  $('#alarm-set-form').submit(function(event) {
-    event.preventDefault();
-    var alarmSet = $('#alarm-set').val();
-    console.log(alarmSet);
-    if (alarmSet === currentTime) {
-      console.log('alarm');
-    }
+  var time = setInterval(function() {
+    checkTime();
+  }, 1000);
 
-  });
+  function checkTime() {
+    $('#time').text(moment().format('hh:mm A'));
+    var currentTime = moment().format('HH:mm');
+    var timeSet = $('#time-set').val();
+    $('#alarm-set-form').submit(function(event) {
+      event.preventDefault();
+    });
+
+    if (timeSet === currentTime) {
+      $('#alarm').text('ALAAAAAAAAAARM');
+    }
+    console.log(timeSet);
+  };
 });
